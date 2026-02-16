6b359d15479540d4493dc0650c29443c27b92fda
Dev2TT <victtor.viltrumita@gmail.com>
2026-02-16 15:33:00 -0300

    Definindo os models(users.py e strong_password.py)

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
+app=Flask(__name__)
 
-def create_app():
-    app=Flask(__name__)
-    app.config.from_object(Config)
-
+def connect_db(aplicacao):
     try:
-        db=SQLAlchemy(app)
-
+        data_base=SQLAlchemy(aplicacao)
     except Exception as e:
         print(f'Não foi possível conectar ao DataBase: {e.__cause__}')
     
+    return data_base
+
+def create_app():
+    app.config.from_object(Config)
+    db=connect_db(app)
     return app
+
